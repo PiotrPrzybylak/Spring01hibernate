6cd18d82370248334be7a9533e77e92dc20e55b2
Piotr Przybylak <piotr.przybylak@gmail.com>
2021-07-06 10:00:31 +0200

    Add Book DAO.

---
+++ src/main/java/pl/coderslab/app/BookDao.java
@@ -0,0 +1,28 @@
+package pl.coderslab.app;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Repository
+@Transactional
+public class BookDao {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    public List<Book> all() {
+        return manager.createQuery("SELECT book FROM Book book").getResultList();
+    }
+
+    public List<Book> findAllByRating(int rating) {
+        Query query = manager.createQuery("SELECT book FROM Book book WHERE book.rating = :rating");
+        query.setParameter("rating", rating);
+        return query.getResultList();
+    }
+
+}
